69fc91ea9b0a8d69f4faf6d94f72b37088e6b8d5
RTurney <r.turney93@gmail.com>
2021-11-04 11:45:54 +0000

    Add fetch test component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+//components
+import DataTest from './components/fetchTest';
 
 function App() {
   return (
@@ -7,6 +9,7 @@ function App() {
       <header className="App-header">
        <h1>Covid Statistics Dashboard</h1>
       </header>
+      <DataTest />
     </div>
   );
 }
