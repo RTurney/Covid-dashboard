179e663ccd0b45b2cbb8e2b4495f69550930b3a2
RTurney <r.turney93@gmail.com>
2021-11-04 14:22:05 +0000

    Finally adding this danm file I keep forgetting

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-
+import React, { useState } from 'react'
 import './App.css';
 //components
-import DataTest from './components/fetchTest';
+import DataTest from './components/FetchTest';
+import Display from './components/Display';
 
 function App() {
+
+  const [display, setDisplay] = useState('');
+
   return (
     <div className="App">
       <header className="App-header">
        <h1>Covid Statistics Dashboard</h1>
       </header>
-      <DataTest />
+      <DataTest setDisplay={setDisplay}/>
+      <Display display={display}/>
     </div>
   );
 }
