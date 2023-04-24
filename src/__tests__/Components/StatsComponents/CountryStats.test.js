d5bcb52875428d147096449c9bd7027d91de70c2
RTurney <r.turney93@gmail.com>
2023-04-24 19:23:33 +0100

    Add countryStats tests

---
+++ src/__tests__/Components/StatsComponents/CountryStats.test.js
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { CountryStats } from "../../../components/StatsComponents/Statistics";
+
+let mockCountryData = [
+    {
+        country: "Rapture",
+        cases: 1200,
+        deaths: 973
+    }
+]
+
+describe("Country Stats", () => {
+    it("displays the country name", () => {
+        render(<CountryStats
+            countryData={mockCountryData}/> )
+        expect(screen.getByText(mockCountryData[0].country)).toBeTruthy();
+    })
+
+    it("displays the total number of cases", () => {
+        render(<CountryStats
+            countryData={mockCountryData}/> )
+        expect(screen.getByText("Total cases: 1200")).toBeTruthy();       
+    })
+
+    it("displays the total number of deaths", () => {
+        render(<CountryStats
+            countryData={mockCountryData}/> )
+        expect(screen.getByText("Total deaths: 973")).toBeTruthy();       
+    })
+})
