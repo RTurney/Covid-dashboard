e08c8a34aca58581e1eed9817e179cbf47046260
RTurney <r.turney93@gmail.com>
2023-04-26 16:59:02 +0100

    Added Husky

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,61 +1,53 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 //css
-import '../styles/NavBar.css' 
+import '../styles/NavBar.css'
 
 const Navbar = ({ setContinentData, setCountryData, setVaccineData }) => {
+  // effects
+  useEffect(() => {
+    checkCountries()
+    // eslint-disable-next-line
+  }, [])
 
-    // effects 
-    useEffect(() => {
-        checkCountries();
-        // eslint-disable-next-line
-    }, []);
+  //functions
+  const checkContinents = () => {
+    setCountryData(null)
+    setVaccineData(null)
+    return fetch('https://disease.sh/v3/covid-19/continents')
+      .then((response) => response.json())
+      .then((data) => setContinentData(data))
+  }
 
-    //functions
-    const checkContinents = () => {
-        setCountryData(null);
-        setVaccineData(null);
-        return (
-            fetch("https://disease.sh/v3/covid-19/continents")
-            .then((response) => response.json())
-            .then((data) => setContinentData(data))
+  const checkCountries = () => {
+    setContinentData(null)
+    setVaccineData(null)
+    return fetch('https://disease.sh/v3/covid-19/countries')
+      .then((response) => response.json())
+      .then((data) => setCountryData(data))
+  };
 
-        )
-    };
-
-    const checkCountries = () => {
-        setContinentData(null);
-        setVaccineData(null);
-        return (
-            fetch("https://disease.sh/v3/covid-19/countries")
-            .then((response) => response.json())
-            .then((data) => setCountryData(data))
-        )
-    }
-
-    const checkVaccines = () => {
-        setContinentData(null);
-        setCountryData(null);
-        return (
-            fetch("https://disease.sh/v3/covid-19/vaccine/coverage/countries?fullData=true")
-            .then((response) => response.json())
-            .then((data) => setVaccineData(data))
-        )
-    }
-
-    return (
-        <div className='nav-Bar'>
-            <h1 className='title'>
-                Global Covid Statistics Dashboard
-            </h1>
-            <div className='navbar-display'>
-                <div className='totals-group'>
-                <button onClick={checkContinents}>Continents data</button>
-                <button onClick={checkCountries}>Countries data</button>
-                <button onClick={checkVaccines}>Vaccines data</button>
-                </div>
-            </div>
-        </div>
+  const checkVaccines = () => {
+    setContinentData(null);
+    setCountryData(null);
+    return fetch(
+      'https://disease.sh/v3/covid-19/vaccine/coverage/countries?fullData=true'
     )
-};
+      .then((response) => response.json())
+      .then((data) => setVaccineData(data));
+  };
 
-export default Navbar;
+  return (
+    <div className="nav-Bar">
+      <h1 className="title">Global Covid Statistics Dashboard</h1>
+      <div className="navbar-display">
+        <div className="totals-group">
+          <button onClick={checkContinents}>Continents data</button>
+          <button onClick={checkCountries}>Countries data</button>
+          <button onClick={checkVaccines}>Vaccines data</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
