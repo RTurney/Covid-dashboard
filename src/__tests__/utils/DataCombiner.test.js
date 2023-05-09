2c717a587cb3d63e86fe0aa8c38d881aa9b4d452
RTurney <r.turney93@gmail.com>
2023-05-09 14:09:07 +0100

    Added tests for combineContinentData function

--- src/__tests__/utils/DataCombiner.test.js
+++ src/__tests__/utils/DataCombiner.test.js
@@ -1,4 +1,4 @@
-import { combineCountryData } from "../../components/utils";
+import { combineCountryData, combineContinentData } from "../../components/utils";
 
 const mockCountryGeoJsonData = [
   {
@@ -35,7 +35,43 @@ const mockCountryAPIData = [
   }
 ]
 
-describe("combineCountryData util ", () => {
+const mockContinentGeoJsonData = [
+  {
+    properties: {
+      continent: "middleearth"
+    },
+    counties: [
+      "Shire",
+      "Gondor",
+      "Rohan",
+      "Mordor"
+    ]
+  },
+  {
+    properties: {
+    continent: "tamriel"
+    },
+    counties: [
+      "Skyrim",
+      "Elsyweir"
+    ]
+  }
+]
+
+const mockContinentAPIData = [
+  {
+    continent: "Middle Earth",
+    cases: 10000,
+    todayCases: 20,
+    deaths: 5000,
+    todayDeaths: 10,
+    casesPerOneMillion: 100,
+    deathsPerOneMillion: 50,
+    population: 1000,
+  }
+]
+
+describe("combineCountryData ", () => {
   const result = combineCountryData(mockCountryAPIData, mockCountryGeoJsonData);
 
 
@@ -50,5 +86,22 @@ describe("combineCountryData util ", () => {
 
   it("will return a country with default parameters if no matching iso3 is found", () => {
     expect(result[1].cases).toEqual("N/A");
+  })
+})
+
+describe("combineContinentData ", () => {
+  const result = combineContinentData(mockContinentAPIData, mockContinentGeoJsonData);
+
+  it("will return an array", () => {
+    expect(Array.isArray(result)).toBeTruthy();
+  })
+
+  it("will return the combined data if continent name's matched", () => {
+    expect(result[0].continent).toEqual(mockContinentAPIData[0].continent);
+    expect(result[0].counties).toEqual(mockContinentGeoJsonData[0].counties);
+  })
+
+  it("will return a continent with default parameters if no continent is matched", () => {
+    expect(result[1].cases).toEqual("N/A")
   })
 })
